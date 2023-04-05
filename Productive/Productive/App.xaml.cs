d08037ede36ad054afa3d5885ca5e042bab9a16c
Aaron Alden <aardalde@ut.utm.edu>
2023-04-05 09:08:13 -0500

    Created EventsSubMenu branch

--- Productive/Productive/App.xaml.cs
+++ Productive/Productive/App.xaml.cs
@@ -13,5 +13,11 @@ namespace Productive
     /// </summary>
     public partial class App : Application
     {
+        private void ShowNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            EventsSubWindow eventsSubWindow = new EventsSubWindow();
+            eventsSubWindow.Show();
+        
+        }
     }
 }
