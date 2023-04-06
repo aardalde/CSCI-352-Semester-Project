51235f0859ece9b0d8be835a25a1c96f11b4fb21
Spencer Karpati <spebkarp@ut.utm.edu>
2023-04-05 20:19:34 -0500

    Added Progress Bar

--- Productive/Productive/MainWindow.xaml.cs
+++ Productive/Productive/MainWindow.xaml.cs
@@ -22,10 +22,10 @@ namespace Productive
     public partial class MainWindow : Window
     {
         DispatcherTimer countdown;
-        TimeSpan time = TimeSpan.FromSeconds(10000);
+        TimeSpan time = TimeSpan.FromSeconds(10);
         double totalTasks = 5;
         double tasksOnTime = 3; //TODO: CHANGE IMPLEMENTAION
-        string eventName = "Event";
+        string eventName = "Event 1";
         int dumbassCount = 0;
 
         public MainWindow()
@@ -33,8 +33,12 @@ namespace Productive
             InitializeComponent();
             countdown = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
             {
-                NextEventBlock.Text = eventName + "\nDue" + "\n" + DateTime.Now.ToString("M") + " " + DateTime.Now.ToString("t") + "\n" + time.ToString("c");
-                if (time == TimeSpan.Zero) countdown.Stop();
+                NextEventBlock.Text = "\n" + eventName + "\nDue" + "\n" + DateTime.Now.ToString("M") + " " + DateTime.Now.ToString("t") + "\n" + "Time Left:\n" + time.ToString("c");
+                if (time == TimeSpan.Zero)
+                {
+                    countdown.Stop();
+                    MessageBox.Show("YOU SUCK", "Seriously?", MessageBoxButton.OK);
+                }
                 time = time.Add(TimeSpan.FromSeconds(-1)); //Countdown timer implementation -> https://stackoverflow.com/questions/16748371/how-to-make-a-wpf-countdown-timer
             }, Application.Current.Dispatcher);
 
@@ -44,37 +48,30 @@ namespace Productive
             ProdRatioBlock.Text = "Productivity Ratio: " + ((tasksOnTime / totalTasks) * 100) + "%";
         }
 
-        private void EventsButton_Click(object sender, RoutedEventArgs e)
+        public void Dumbass()
         {
             dumbassCount++;
             MessageBox.Show("You're not supposed to be here", "Go Away", MessageBoxButton.OK);
-            if(dumbassCount == 5)
+            if (dumbassCount == 5)
             {
                 MessageBox.Show("Alright buddy you lost your application privileges", "GET OUT", MessageBoxButton.OK);
                 System.Environment.Exit(0);
             }
+        }
+
+        private void EventsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Dumbass();
         }
 
         private void ImportButton_Click(object sender, RoutedEventArgs e)
         {
-            dumbassCount++;
-            MessageBox.Show("You're not supposed to be here", "Go Away", MessageBoxButton.OK);
-            if (dumbassCount == 5)
-            {
-                MessageBox.Show("Alright buddy you lost your application privileges", "GET OUT", MessageBoxButton.OK);
-                System.Environment.Exit(0);
-            }
+            Dumbass();
         }
 
         private void ExportButton_Click(object sender, RoutedEventArgs e)
         {
-            dumbassCount++;
-            MessageBox.Show("You're not supposed to be here", "Go Away", MessageBoxButton.OK);
-            if (dumbassCount == 5)
-            {
-                MessageBox.Show("Alright buddy you lost your application privileges", "GET OUT", MessageBoxButton.OK);
-                System.Environment.Exit(0);
-            }
+            Dumbass();
         }
     }
 }
