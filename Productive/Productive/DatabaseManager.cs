06a6f218a716ae3496252122a42ff921721e3d3c
Aaron Alden <aardalde@ut.utm.edu>
2023-04-13 16:50:04 -0500

    Added a class to handle all communication with the database

---
+++ Productive/Productive/DatabaseManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productive
+{
+    internal class DatabaseManager //This class will be responsible for all interactions with the relational database (e.g., storing the users username and password)
+    {
+        public void CreateUser(string username, string password)
+        {
+            //A lot of thinking involved
+        }
+
+    }
+}
