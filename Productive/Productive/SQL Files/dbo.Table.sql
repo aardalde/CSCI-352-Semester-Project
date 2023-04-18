ccbda22ea849112fa29ec82fd49f23c2fc47a680
Aaron Alden <aardalde@ut.utm.edu>
2023-04-18 08:44:22 -0500

    Delete SQL Files folder

--- Productive/Productive/SQL Files/dbo.Table.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Users]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Username] VARCHAR(50) NOT NULL, 
-    [Password] VARCHAR(50) NOT NULL,
-)
--- Productive/Productive/SQL Files/dbo.Table_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Events]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [UserID] INT NOT NULL, 
-    [EventDescription] VARCHAR(MAX) NOT NULL, 
-    [DueDate] DATETIME2 NOT NULL
-	FOREIGN KEY (UserId) REFERENCES Users(Id)
-)
