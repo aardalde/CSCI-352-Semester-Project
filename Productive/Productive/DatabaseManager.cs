cf78588db0d07af22dd3ab0e72a16033c69adeb8
Aaron Alden <aardalde@ut.utm.edu>
2023-04-20 16:26:54 -0500

    Added basic login logic (creating an account, checking if username already exists, etc.)

--- Productive/Productive/DatabaseManager.cs
+++ Productive/Productive/DatabaseManager.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
 using System.Windows.Markup;
 
 namespace Productive
@@ -12,15 +14,42 @@ namespace Productive
     {
         public static bool InsertUser(string username, string password)
         {
-            using (SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Integrated Security=True"))
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Aaron\Source\Repos\aardalde\CSCI-352-Semester-Project\Productive\Productive\PPMDatabase.mdf;Integrated Security=True;Connect Timeout=30"))
             {
                 connection.Open();
-                SqlCommand command = new SqlCommand("INSERT INTO Users (Username, Password) VALUES (@username, @password)", connection);
+
+                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Users WHERE Username = @username", connection);
                 command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@password", password);
-                int rowsAffected = command.ExecuteNonQuery();
-                return rowsAffected > 0;
+
+                int count = (int)command.ExecuteScalar();
+
+                if (count > 0)
+                {
+                    return false; // User already exists
+                }
+                else
+                {
+                    command = new SqlCommand("INSERT INTO Users (Username, Password) VALUES (@username, @password)", connection);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    return rowsAffected > 0; // Return true if the user was successfully inserted
+                }
             }
+        }
+
+        public static bool CheckUserExists(SqlConnection connection, string username, string password)
+        {
+            string query = "SELECT COUNT(*) FROM Users WHERE Username = @Username AND Password = @Password";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@Username", username);
+            command.Parameters.AddWithValue ("@Password", password);
+
+            int result = (int)command.ExecuteScalar();
+            return (result > 0);
         }
     }
 }
+
--- Productive/Productive/UserLogin.xaml.cs
+++ Productive/Productive/UserLogin.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -28,7 +30,40 @@ namespace Productive
             string username = UsernameTextBox.Text;
             string password = PasswordTextBox.Text;
 
-            DatabaseManager.InsertUser(username, password);
+            if (DatabaseManager.InsertUser(username, password))
+            {
+                MessageBox.Show("Account successfully created");
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Account already exists with this username, please login or try a different username");
+            }
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            using (SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Aaron\Source\Repos\aardalde\CSCI-352-Semester-Project\Productive\Productive\PPMDatabase.mdf;Integrated Security=True;Connect Timeout=30"))
+            {
+                connection.Open();
+
+                if (DatabaseManager.CheckUserExists(connection, username, password))
+                {
+                    MessageBox.Show("Login successful");
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("No account found, please create an account");
+                }
+            }
         }
     }
 }
